7a7266e1041c6b2535b3d1d77dac0dccfca3b999
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-03 20:26:18 +0100

    Autogit: 03-04-2020 20:26

--- material/src/App.js
+++ material/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 function App() {
   return <Container maxWidth="sm"></Container>;
