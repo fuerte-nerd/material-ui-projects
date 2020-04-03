2513175f9be7f4cf971a2cc92464a6852c49b7b6
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-03 20:36:28 +0100

    Autogit: 03-04-2020 20:36

--- material/src/App.js
+++ material/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
+import Helmet from "react-helmet";
+
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
 function App() {
   return (
-    <Box bgcolor="black">
-      <Container maxWidth="sm"></Container>
-    </Box>
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Helmet>
+      <Box>
+        <Container>
+          <Typography variant="h1">A simple todo app</Typography>
+        </Container>
+      </Box>
+    </>
   );
 }
 
