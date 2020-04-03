3f80b14f1b33443727089ddaeb7bc15803473ac4
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-03 21:51:34 +0100

    Autogit: 03-04-2020 21:51

--- material/src/App.js
+++ material/src/App.js
@@ -6,6 +6,8 @@ import Helmet from "react-helmet";
 
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 import Box from "@material-ui/core/Box";
 
 function App() {
@@ -17,6 +19,18 @@ function App() {
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
       </Helmet>
+      <Fab
+        color="primary"
+        aria-label="add"
+        style={{
+          position: "fixed",
+          top: "2rem",
+          right: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <AddIcon />
+      </Fab>
       <Box>
         <Container>
           <Typography variant="h1">A simple todo app</Typography>
