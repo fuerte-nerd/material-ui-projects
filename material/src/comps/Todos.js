0e28e2acd7630823f18e0e0c2afccc6341bcea8a
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-03 22:06:54 +0100

    Autogit: 03-04-2020 22:06

--- material/src/comps/Todos.js
+++ material/src/comps/Todos.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 const Todos = () => {
+  const dummyData = [
+    {
+      id: 1,
+      title: "First Todo",
+      done: false,
+      create_date: new Date(2020, 3, 2),
+      complete_date: null,
+    },
+    {
+      id: 2,
+      title: "Second Todo",
+      done: false,
+      create_date: new Date(2020, 3, 2),
+      complete_date: null,
+    },
+  ];
   return (
     <div>
+      {dummyData.map((i) => {
+        return <h2>{i.title}</h2>;
+      })}
+
       <h1>I am the todos</h1>
     </div>
   );
