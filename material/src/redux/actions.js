24c89ea64eb60b4d6639772e7f43430b80b99adf
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-04 13:06:25 +0100

    Autogit: 04-04-2020 13:06

--- material/src/redux/actions.js
+++ material/src/redux/actions.js
@@ -1,1 +1,21 @@
 import { ADD_TODO, DELETE_TODO, TOGGLE_DONE } from "./types";
+
+export const addTodo = (todo) => {
+  return {
+    type: ADD_TODO,
+    payload: todo,
+  };
+};
+export const deleteTodo = (todo) => {
+  return {
+    type: DELETE_TODO,
+    payload: todo,
+  };
+};
+
+export const toggleDone = (todo) => {
+  return {
+    type: TOGGLE_DONE,
+    payload: todo,
+  };
+};
