3320228dee8be579fd76170480df8f45d6d4ddc9
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-05 15:51:01 +0100

    Autogit: 05-04-2020 15:51

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./comps/Header";
+import Main from "./comps/Main";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 }
