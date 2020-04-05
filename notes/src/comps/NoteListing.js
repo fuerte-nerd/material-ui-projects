77b473bc6f308b1770d52552830cb6b99ee1dcec
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-05 18:07:42 +0100

    Autogit: 05-04-2020 18:07

--- notes/src/comps/NoteListing.js
+++ notes/src/comps/NoteListing.js
@@ -10,13 +10,19 @@ import {
 
 const NoteListing = (props) => {
   const { id, title, locked, create_date, modified_date, body } = props.data;
+
+  const excerpt = () => {
+    return body.length > 150 ? body.substr(0, 150) + "..." : body;
+  };
+
   return (
     <Grid item xs={12} md={6}>
       <Card style={{ marginBottom: "1rem" }}>
         <CardContent>
-          <Typography variant="h4" variantMapping={{ h4: "h2" }}>
+          <Typography variant="h4" variantMapping={{ h4: "h2" }} gutterBottom>
             {title}
           </Typography>
+          <Typography variant="body1">{excerpt()}</Typography>
         </CardContent>
         <CardActions>
           <Button>Open</Button>
