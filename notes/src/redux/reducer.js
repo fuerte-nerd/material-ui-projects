b36cf68b235eec872f7a373aee0f0a769ce9489a
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-06 11:48:10 +0100

    Autogit: 06-04-2020 11:48

---
+++ notes/src/redux/reducer.js
@@ -0,0 +1,32 @@
+import { ADD_NOTE, TOGGLE_VIEWER, LOAD_NOTE, UPDATE_NOTE, DELETE_NOTE } from "./types"
+
+const initialState = {
+  isViewerOpen: false,
+  noteLoaded: null,
+  notes: [],
+}
+
+export default = (state = initialState, action)=>{
+  switch(action.type){
+    case ADD_NOTE:
+      return {
+        ...state
+      }
+    case LOAD_NOTE:
+      return {
+        ...state
+      }
+    case DELETE_NOTE:
+      return {
+        ...state
+      }
+    case UPDATE_NOTE:
+      return {
+        ...state
+      }
+    case TOGGLE_VIEWER:
+      return {
+        ...state
+      }
+  }
+}
