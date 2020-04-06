e8f4e77e20482bc493c823eddc7f0d6f544b4f9c
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-06 11:42:59 +0100

    Autogit: 06-04-2020 11:42

---
+++ notes/src/redux/types.js
@@ -0,0 +1,5 @@
+export const TOGGLE_VIEWER = "TOGGLE_VIEWER";
+export const LOAD_NOTE = "LOAD_NOTE";
+export const UPDATE_NOTE = "UPDATE_NOTE";
+export const DELETE_NOTE = "DELETE_NOTE";
+export const ADD_NOTE = "ADD_NOTE";
