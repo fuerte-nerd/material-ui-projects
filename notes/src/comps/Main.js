b3672a1ed4c728ff5fa2fbe8de7a9029654e355a
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-06 09:26:01 +0100

    Autogit: 06-04-2020 09:26

--- notes/src/comps/Main.js
+++ notes/src/comps/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import NoteListings from "./NoteListings";
+import NoteViewer from "./NoteViewer";
 
 const Main = () => {
   return (
     <main style={{ margin: "2rem 0" }}>
       <NoteListings />
+      <NoteViewer />
     </main>
   );
 };
