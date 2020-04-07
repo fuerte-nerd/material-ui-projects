953b04afb1f12ba68ad72ffb02350c7e34253aad
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-07 14:10:40 +0100

    Autogit: 07-04-2020 14:10

--- notes/src/comps/NoteListings.js
+++ notes/src/comps/NoteListings.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import NoteListing from "./NoteListing";
-import { Grid, Container } from "@material-ui/core";
+import { Grid, Container, Typography } from "@material-ui/core";
 
 import dummyData from "../dummyData";
 
 const NoteListings = (props) => {
   return (
     <Container>
-      {props.notes && props.notes.length > 1
-        ? props.notes.map((i) => {
-            return (
-              <Grid container spacing={4}>
-                <NoteListing id={i.id} />
-              </Grid>
-            );
-          })
-        : null}
+      {props.notes && props.notes.length > 1 ? (
+        props.notes.map((i) => {
+          return (
+            <Grid container spacing={4}>
+              <NoteListing id={i.id} />
+            </Grid>
+          );
+        })
+      ) : (
+        <Typography p={6}>You don't have any notes yet...</Typography>
+      )}
     </Container>
   );
 };
