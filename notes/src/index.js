b25f2ac43a4f3ea7e9e249c4e16154f0ff1e6a74
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-08 14:12:57 +0100

    Autogit: 08-04-2020 14:12

--- notes/src/index.js
+++ notes/src/index.js
@@ -5,10 +5,10 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.render(
-  <React>
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
