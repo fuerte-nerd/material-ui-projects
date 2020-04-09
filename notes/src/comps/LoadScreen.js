dc8b66e4626540e184936f4cefb4ef3cbe8549f0
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-09 15:02:11 +0100

    Autogit: 09-04-2020 15:02

--- notes/src/comps/LoadScreen.js
+++ notes/src/comps/LoadScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Dialog } from "@material-ui/core";
 const LoadScreen = () => {
-  return <Dialog fullScreen open={true}></Dialog>;
+  return (
+    <Dialog
+      fullScreen
+      open={true}
+      transitionDuration={0}
+      disableEscapeKeyDown
+      disableBackdropClick
+    ></Dialog>
+  );
 };
 
 export default LoadScreen;
