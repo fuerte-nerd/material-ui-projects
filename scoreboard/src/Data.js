df07e336429431f4b0ee4c28ff9f4fb0bb4dd513
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-11 10:13:38 +0100

    Autogit: 11-04-2020 10:13

---
+++ scoreboard/src/Data.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    id: 1,
+    title: "My brilliant game!",
+    date_created: new Date(),
+    date_modified: new Date(),
+    players: [
+      {
+        name: "John",
+        score: 10,
+      },
+      {
+        name: "Linda",
+        score: 20,
+      },
+      {
+        name: "Harry",
+        score: 50,
+      },
+      {
+        name: "Howard",
+        score: 0,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "My brilliant second game!",
+    date_created: new Date(),
+    date_modified: new Date(),
+    players: [
+      {
+        name: "Dan",
+        score: 10,
+      },
+      {
+        name: "Alan",
+        score: 5,
+      },
+      {
+        name: "Brian",
+        score: 2,
+      },
+    ],
+  },
+];
