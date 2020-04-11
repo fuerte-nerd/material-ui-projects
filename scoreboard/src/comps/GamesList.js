c4bfa68392f0c6a0d812a971340cc7e67085b69f
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-11 15:51:42 +0100

    Autogit: 11-04-2020 15:51

--- scoreboard/src/comps/GamesList.js
+++ scoreboard/src/comps/GamesList.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { GamesListEntry } from "../Components";
 
-import { Grid } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 
 import Data from "../Data";
 
 const GamesList = () => {
   return (
-    <Grid container spacing={2}>
-      {Data.map((i) => {
-        return (
-          <Grid item xs={12} md={6} lg={4}>
-            <GamesListEntry gameData={i} />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Box mt={3}>
+      <Grid container spacing={2}>
+        {Data.map((i) => {
+          return (
+            <Grid item xs={12} md={6} lg={4}>
+              <GamesListEntry gameData={i} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 };
 
