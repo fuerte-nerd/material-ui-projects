f3a9fbc4ba10f7614efab06233f49468c24b9710
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-12 11:45:04 +0100

    Autogit: 12-04-2020 11:45

--- scoreboard/src/comps/EditPlayer.js
+++ scoreboard/src/comps/EditPlayer.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { Dialog, Typography } from "@material-ui/core";
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
 const EditPlayer = () => {
   return (
     <Dialog open={true}>
+      <DialogTitle>Edit Player</DialogTitle>
+      <DialogContent>
+        <TextField />
+        <TextField type="number" />
+      </DialogContent>
+      <DialogActions>
+        <Button></Button>
+        <Button></Button>
+      </DialogActions>
       <Typography>Hello</Typography>
     </Dialog>
   );
