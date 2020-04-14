afd99774ea38decc9f43f40b74c3e603b93cfdde
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-14 12:10:35 +0100

    Autogit: 14-04-2020 12:10

--- scoreboard/src/comps/NewGame.js
+++ scoreboard/src/comps/NewGame.js
@@ -5,16 +5,30 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
+  TextField,
+  Grid,
 } from "@material-ui/core";
 
 const NewGame = () => {
   return (
-    <Dialog fullScreen open>
+    <Dialog maxWidth="md" open>
       <DialogTitle>Hello</DialogTitle>
-      <DialogContent>Yes</DialogContent>
+      <DialogContent>
+        <Grid container spacing={2}>
+          <Grid xs={12} item>
+            <TextField label="Title" id="title" fullWidth />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <TextField label="Description" id="description" />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <TextField label="No. of players" id="players" />
+          </Grid>
+        </Grid>
+      </DialogContent>
       <DialogActions>
-        <Button></Button>
-        <Button></Button>
+        <Button>Cancel</Button>
+        <Button>Create</Button>
       </DialogActions>
     </Dialog>
   );
