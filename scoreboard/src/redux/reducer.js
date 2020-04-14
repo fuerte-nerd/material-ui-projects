a37439b7e07f4eeca5882aba3edf8e0fbe897e19
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-14 14:37:02 +0100

    Autogit: 14-04-2020 14:37

--- scoreboard/src/redux/reducer.js
+++ scoreboard/src/redux/reducer.js
@@ -3,10 +3,18 @@ import { TOGGLE_DIALOG, CREATE_NEW_GAME, UPDATE_GAME } from "./types";
 const initialState = {
   games: [],
   gameLoadedInViewer: null,
-  dialogs: {
-    viewer: false,
-    newGame: false,
-    addPlayer: false,
-    editPlayer: false,
-  },
+  viewerDialog: false,
+  newGameDialog: false,
+  addPlayerDialog: false,
+  editPlayerDialog: false
 };
+
+export default  (state=initialState, action)=>{
+  switch(action.type){
+    case TOGGLE_DIALOG:
+      return {
+        ...state,
+        [action.payload]: !state[action.payload]
+  }
+
+}
