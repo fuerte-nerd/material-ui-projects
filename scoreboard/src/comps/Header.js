3e5a3e16543d017243eea52326f3d349fb1a0561
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-15 09:11:42 +0100

    Autogit: 15-04-2020 09:11

--- scoreboard/src/comps/Header.js
+++ scoreboard/src/comps/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { toggleDialog } from "../redux/actions";
 
 import { Button, AppBar, Toolbar, Typography } from "@material-ui/core";
 
-const Header = () => {
+const Header = (props) => {
+  const handleClick = () => {
+    props.dispatch(toggleDialog("newGameDialog"));
+  };
   return (
     <>
       <AppBar>
         <Toolbar>
           <Typography style={{ flex: 1 }}>Scoreboard</Typography>
-          <Button color="inherit" edge="end">
+          <Button color="inherit" edge="end" onClick={handleClick}>
             New
           </Button>
         </Toolbar>
@@ -18,4 +24,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default connect()(Header);
