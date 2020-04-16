e613305e63e7834e5725a5523bfcb589a7eff1f1
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-16 12:48:42 +0100

    Autogit: 16-04-2020 12:48

--- scoreboard/src/Components.js
+++ scoreboard/src/Components.js
@@ -3,7 +3,6 @@ import Footer from "./comps/Footer";
 import Main from "./comps/Main";
 import GamesList from "./comps/GamesList";
 import GamesListEntry from "./comps/GamesListEntry";
-import Game from "./comps/Game";
 import ScoreboardViewer from "./comps/ScoreboardViewer";
 import EditPlayer from "./comps/EditPlayer";
 import AddPlayer from "./comps/AddPlayer";
@@ -17,7 +16,6 @@ export {
   Main,
   GamesList,
   GamesListEntry,
-  Game,
   ScoreboardViewer,
   EditPlayer,
   AddPlayer,
