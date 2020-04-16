4ea029e67d14f5ce3ca2d792b64114f95abc4f3a
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-16 14:39:27 +0100

    Autogit: 16-04-2020 14:39

--- scoreboard/src/comps/ScoreboardPlayerEntry.js
+++ scoreboard/src/comps/ScoreboardPlayerEntry.js
@@ -4,6 +4,18 @@ import { TableRow, TableCell, IconButton } from "@material-ui/core";
 import { RemoveCircle, AddCircle } from "@material-ui/icons";
 
 const ScoreboardPlayerEntry = (props) => {
+  const handleClick = (e) => {
+    const f = e.currentTarget;
+    switch (f.id) {
+      case "inc-score":
+        return console.log(`increase score for ${props.name} clicked`);
+      case "dec-score":
+        return console.log(`increase score for ${props.name} clicked`);
+      default:
+        return console.log(`the row was clicked`);
+    }
+    console.log("clicked");
+  };
   return (
     <TableRow hover key={props.id} id={props.id}>
       <TableCell id={`pos_${props.id}`} onClick={handleClick}>
