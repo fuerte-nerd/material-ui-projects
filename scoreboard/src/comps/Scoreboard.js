8c42d1ee5eb8c9e5e59c2b226fe0664618cbfc35
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-16 09:56:36 +0100

    Autogit: 16-04-2020 09:56

--- scoreboard/src/comps/Scoreboard.js
+++ scoreboard/src/comps/Scoreboard.js
@@ -1,50 +1,73 @@
-<TableContainer component={Paper}>
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell width="10%">Pos.</TableCell>
-        <TableCell width="50%">Name</TableCell>
-        <TableCell width="30%" align="right">
-          Score
-        </TableCell>
-        <TableCell width="5%" align="center" padding="none">
-          <Box mt={1}>
-            <Icon>
-              <RemoveCircle />
-            </Icon>
-          </Box>
-        </TableCell>
-        <TableCell width="5%" align="center" padding="none">
-          <Box mt={1}>
-            <Icon>
-              <AddCircle />
-            </Icon>
-          </Box>
-        </TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {typeof compState.players === "object"
-        ? compState.players.map((i, ind) => {
-            return (
-              <TableRow>
-                <TableCell>{getPosition(ind)}</TableCell>
-                <TableCell>{i.name}</TableCell>
-                <TableCell align="right">{i.score}</TableCell>
-                <TableCell align="center" padding="none">
-                  <IconButton>
-                    <RemoveCircle />
-                  </IconButton>
-                </TableCell>
-                <TableCell align="center" padding="none">
-                  <IconButton>
-                    <AddCircle />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            );
-          })
-        : null}
-    </TableBody>
-  </Table>
-</TableContainer>;
+import React, { useState } from "react";
+
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  Box,
+  Icon,
+  IconButton,
+  Paper,
+  TableBody,
+} from "@material-ui/core";
+
+import { RemoveCircle, AddCircle } from "@material-ui/icons";
+
+const Scoreboard = (props) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell width="10%">Pos.</TableCell>
+            <TableCell width="50%">Name</TableCell>
+            <TableCell width="30%" align="right">
+              Score
+            </TableCell>
+            <TableCell width="5%" align="center" padding="none">
+              <Box mt={1}>
+                <Icon>
+                  <RemoveCircle />
+                </Icon>
+              </Box>
+            </TableCell>
+            <TableCell width="5%" align="center" padding="none">
+              <Box mt={1}>
+                <Icon>
+                  <AddCircle />
+                </Icon>
+              </Box>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {typeof compState.players === "object"
+            ? compState.players.map((i, ind) => {
+                return (
+                  <TableRow>
+                    <TableCell>{getPosition(ind)}</TableCell>
+                    <TableCell>{i.name}</TableCell>
+                    <TableCell align="right">{i.score}</TableCell>
+                    <TableCell align="center" padding="none">
+                      <IconButton>
+                        <RemoveCircle />
+                      </IconButton>
+                    </TableCell>
+                    <TableCell align="center" padding="none">
+                      <IconButton>
+                        <AddCircle />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                );
+              })
+            : null}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Scoreboard;
